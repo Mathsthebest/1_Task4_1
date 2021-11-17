a49e0752bd3dd41bee75de3d546309f266fda8a8
dowsr <dowsr@LAPTOP-H00V0KT0>
2021-11-17 23:15:13 +0300

    Добавьте файлы проекта.

---
+++ 1_Task4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int S = 0;
+            int n = 2 * N - 1;
+
+            for (int i = 1; i <= n; i += 2)
+            {
+                S += i;
+                Console.WriteLine(S);
+            }
+            Console.ReadKey();
+        }
+    }
+}
